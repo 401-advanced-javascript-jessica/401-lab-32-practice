b42f33a7c2f2493432917809cd6ad761949d5aec
jessicakwalters <jessicakwalters@gmail.com>
2019-09-26 13:25:00 -0700

    lint, docs, readme

---
+++ client/styleguide/precache-manifest.85651607b34a92430b29c4a65f69356f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080f181dd33ce017945f",
+    "url": "build/2.080f181d.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "721d9a00a3de5751a7f8",
+    "url": "build/main.721d9a00.js"
+  },
+  {
+    "revision": "4f2b2963fe43e405bdc9aaec499b590c",
+    "url": "index.html"
+  }
+]);
